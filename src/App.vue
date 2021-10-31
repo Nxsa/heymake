c42ab832d13c9cc6c8511701ddc71c062c4f9efc
Nosa Onaiwu <officiallyprime@gmail.com>
2021-10-31 10:25:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<div id="app" class="wrapper">
+		<div id="footer-control">
+			<transition name="fade" mode="out-in">
+				<router-view></router-view>
+			</transition>
+		</div>
+	</div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="pg-index">
+		<logo />
+	</div>
+</template>
+
+<script>
+import Logo from "@/vectors/Logo.vue";
+export default {
+	name: "Index",
+	components: {
+		Logo,
+	},
+	metaInfo: {
+		title: "Heymake!",
+		titleTemplate: "%s - Think. Make. Create",
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
